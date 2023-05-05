01152b09ad1728ad9f02db116304abd8fcbadf0f
jessarr10 <78123393+jessarr10@users.noreply.github.com>
2023-05-05 17:45:30 -0400

    Update password_guess.py

--- password_guess.py
+++ password_guess.py
@@ -7,15 +7,15 @@ from sklearn.model_selection import train_test_split
 #adding test data and spliting data
 df = pd.read_csv("data.csv")
 
-x = df.iloc[:25000, :-1].values
-y = df.iloc[:25000, -1].values
+x = df.iloc[10000:35000, :-1].values
+y = df.iloc[10000:35000, -1].values
 
 encoder = OneHotEncoder(sparse_output=False)
 x = encoder.fit_transform(x.reshape(-1, 1))
 y = tf.keras.utils.to_categorical(y, num_classes=3)
 
 #split model into testing and training data
-x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=.5, random_state=0)
+x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=.6, random_state=0)
 x_valid, x_test, y_valid, y_test = train_test_split(x_temp, y_temp, test_size=.4, random_state=0)
 
 #build model
@@ -30,11 +30,11 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-#evaluate with training data
-model.evaluate(x_train, y_train)
+#evaluate with test data before training
+model.evaluate(x_test, y_test)
 
 #train data - want accuracy to increase, loss to decrease
 model.fit(x_train, y_train, batch_size=16, epochs=10)
 
-#evaluate with test data
+#evaluate with test data after training 
 model.evaluate(x_test, y_test)
